50bca6c6fa504966878be8e095fe22d9c3edb01e
PoojaMaddiwar <poojamaddiwar23@gmail.com>
2024-04-27 00:36:08 +0530

    get functionality is done

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,12 +1,21 @@
-// mongodb+srv://admin:<password>@cluster0.xiwcfcs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
 
 const express = require("express");
 const cors = require("cors");
 const {MongoClient} = require("mongodb");
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 const PortFolio = new MongoClient("mongodb+srv://admin:admin@cluster0.xiwcfcs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+
+// get request
+app.get("/education",async(req,res)=>{
+    await PortFolio.connect();
+    let arr = await PortFolio.db("my_portfolio").collection("education").find().toArray();
+    res.json(arr);
+})
 
 
 app.listen(8080,()=>{
