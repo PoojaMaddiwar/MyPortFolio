7299aeb4366bbbbb34896e2b49a4ac5ac65a2f5c
PoojaMaddiwar <poojamaddiwar23@gmail.com>
2024-04-27 00:34:41 +0530

    successfully geting data from database

--- My_Porfolio/src/components/Education.jsx
+++ My_Porfolio/src/components/Education.jsx
@@ -1,11 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const Education = ()=>{
+    const [details,setdetails] = useState([])
+    useEffect(()=>{
+        axios.get("http://localhost:8080/education").then((posRes)=>{
+            const {data} = posRes;
+            setdetails(data);
+            console.log(data)
+
+        },(eroRes)=>{
+            console.log(eroRes)
+        })
+    },[])
     return(
-        <>
+       <>
         <div className="bg-containerr">
-            <div class="card w-25 col-md-6">
-                <div class="card-body">
-                    <h5 class="card-title"><i class="fa fa-calendar-day"></i>Card title</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <div className="card w-25 col-md-6">
+                <div className="card-body">
+                    <h5 className="card-title"><i className="fa fa-calendar-day"></i>Card title</h5>
+                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
    
                 </div>
             </div>
