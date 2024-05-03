1efed086ca957516830ae457945cd2b455d28303
PoojaMaddiwar <poojamaddiwar23@gmail.com>
2024-05-04 01:33:08 +0530

    about page content

--- My_Porfolio/src/components/About/About.jsx
+++ My_Porfolio/src/components/About/About.jsx
@@ -1,7 +1,42 @@
+import abouImg from "../../assets/about/aboutImage.png"
+import "./about.css"
 const About = ()=>{
     return(
         <>
-        <h3>About component</h3>
+        <section className="abtcontainer">
+            <h2 className="abttitle">About</h2>
+
+            <div className="abtcontent">
+                <img src={abouImg} 
+                alt="Me sitting with laptop"
+                className="aboutImage" />
+
+                <ul className="aboutItem">
+                    <li className="aboutItem">
+                        <img src="" alt="" />
+                        <div className="aboutItemText">
+                            <h3>Frontend Developer</h3>
+                            <p></p>
+                        </div>
+                    </li>
+                    <li className="aboutItem">
+                        <img src="" alt="" />
+                        <div className="aboutItemText">
+                            <h3></h3>
+                            <p></p>
+                        </div>
+                    </li>
+                    <li className="aboutItem">
+                        <img src="" alt="" />
+                        <div className="aboutItemText">
+                            <h3></h3>
+                            <p></p>
+                        </div>
+                    </li>
+                </ul>
+
+            </div>
+        </section>
         </>
     )
 }
