8051e5f78d0583b91dddf4760acad063d41eab08
PoojaMaddiwar <poojamaddiwar23@gmail.com>
2024-06-23 17:19:17 +0530

    backend is done

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,12 +1,14 @@
 
-
+require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const {MongoClient} = require("mongodb");
+const nodemailer = require("nodemailer");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 
 const PortFolio = new MongoClient("mongodb+srv://admin:admin@cluster0.xiwcfcs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
